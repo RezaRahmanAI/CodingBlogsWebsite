0c4db90a28a2f4ffc5486b491fabc5732ccf6768
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2024-07-06 01:18:22 +0600

    Add project files.

---
+++ Models/Domain/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿namespace CodingBlogs.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SortDescription { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public string UrlHandle { get; set; }
+        public string FeaturedImgUrl { get; set; }        
+        public DateTime CreatedDate { get; set; }        
+        public bool IsVisible { get; set; }
+
+    }
+}
---
+++ Models/Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingBlogs.API.Models.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
